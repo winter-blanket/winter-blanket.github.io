e9e62ebccefceafef34578defa369720b06b7a06
하미연 <miyeon.ha.22@gmail.com>
2023-06-25 19:41:17 +0900

    Deploy website - based on b50b7154b927e9177098346ef4b7d812eff6d4b0

--- custom_script.js
+++ custom_script.js
@@ -1,10 +1,14 @@
 console.log('custom script works')
 
 
-var btn_product_titles = document.querySelectorAll('.btn-product-title')
+if (typeof window !== 'undefined'){
+
+  var btn_product_titles = document.querySelectorAll('.btn-product-title')
+console.log(btn_product_titles)
 
 btn_product_titles.forEach(function (btn) {
   btn.addEventListener('click', function handleProductTitleClick (event) {
+    console.log('check')
     if (event.target.classList.contains('clicked')) {
       event.target.classList.remove('clicked')
       event.target.parentElement.parentElement.nextElementSibling.classList.remove(
@@ -21,10 +25,21 @@ btn_product_titles.forEach(function (btn) {
 
 
 
-const blog_post_cards = document.querySelectorAll('.blog-list-page article')
 
-blog_post_cards.forEach(function (card){
+}
 
-  console.log(card)
 
-})
+
+console.log('check22')
+console.log('check33')
+
+
+
+// const blog_post_cards = document.querySelectorAll('.blog-list-page article')
+// console.log(blog_post_cards)
+
+// blog_post_cards.forEach(function (card){
+
+//   console.log(card)
+
+// })
